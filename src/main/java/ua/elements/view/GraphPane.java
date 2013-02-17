c6f8035c82271153fbf90d582dbba9082a4b8e50
Vitaliy <plohoi.chelovek@gmail.com>
2013-02-17 15:16:39 +0200

    begin working on graph

---
+++ src/main/java/ua/elements/view/GraphPane.java
@@ -0,0 +1,10 @@
+package ua.elements.view;
+
+import org.eclipse.swt.*;
+import org.eclipse.swt.widgets.*;
+
+public class GraphPane extends Composite {
+    public GraphPane(Composite parent, int style) {
+	super(parent, style);
+    }
+}
